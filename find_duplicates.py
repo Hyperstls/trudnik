6c6bcf55b8ef72a230c7e7b6732c083d5ec0df44
Hyperstls <prcp2@inbox.ru>
2026-06-03 15:43:19 +0300

    "cleanup-remove-test-scripts"

--- find_duplicates.py
+++
@@ -1,6 +0,0 @@
-lines = open('app.py', encoding='utf-8').readlines()
-matches = []
-for i, line in enumerate(lines):
-    if 'my-applications' in line.lower():
-        matches.append(f'{i+1}: {line.rstrip()}')
-print('\n'.join(matches))
