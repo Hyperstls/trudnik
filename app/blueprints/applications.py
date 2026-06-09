087f136e767b760d5b1220270837a83206868c4e
Hyperstls <prcp2@inbox.ru>
2026-06-09 13:39:23 +0300

    debug: логировать app_id в api_handle_application для диагностики

--- app/blueprints/applications.py
+++ app/blueprints/applications.py
@@ -167,8 +167,10 @@ def api_test():
 
 def api_handle_application(app_id, action):
     """AJAX-эндпоинт: принять / отклонить / повторно принять отклик"""
+    current_app.logger.info('[APPLICATIONS] action=%s app_id=%s', action, app_id)
     app_resp = supabase_request('GET', f'applications?id=eq.{app_id}&select=job_id,worker_id,status,shift_id')
     if not app_resp.ok or not app_resp.json():
+        current_app.logger.warning('[APPLICATIONS] app_id=%s not found (ok=%s, has_json=%s)', app_id, app_resp.ok, bool(app_resp.json() if app_resp.ok else False))
         return jsonify({'success': False, 'error': 'Отклик не найден'}), 404
 
     app_data = app_resp.json()[0]
